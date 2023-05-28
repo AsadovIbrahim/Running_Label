8303a627076e35dbdf1094ceba0d167f2dfd5c10
Ibrahim <ibrahimasadov32@gmail.com>
2023-05-28 16:22:37 +0400

    Running_Label

---
+++ Form1.Designer.cs
@@ -0,0 +1,35 @@
+﻿namespace First_Task
+{
+    partial class Form1
+    {
+        
+        private System.ComponentModel.IContainer components = null;
+       
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1075, 581);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿namespace First_Task
+{
+    public partial class Form1 : Form
+    {
+        private Label label1;
+        public Form1()
+        {
+            InitializeComponent();
+            label1 = new Label
+            {
+                Location = new Point(50, 50),
+                Size = new Size(100, 100),
+                BorderStyle = BorderStyle.FixedSingle,
+                BackColor = Color.Red,
+            };
+
+            label1.MouseHover += Label1_MouseHover;
+
+            Controls.Add(label1);
+        }
+        private void Label1_MouseHover(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int x = random.Next(0, Width - label1.Width);
+            int y = random.Next(0, Height - label1.Height);
+
+            label1.Location = new Point(x, y);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
+
